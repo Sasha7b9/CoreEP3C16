d9e7b0f0b02caab9c633080e1f4430cb63d259bc
Sasha7b9 <Sasha7b9@tut.by>
2022-05-06 23:47:59 +0300

    *

--- sources/Verilog/8 Push Buttons/joystick.v
+++ sources/Verilog/8 Push Buttons/joystick.v
@@ -1,23 +1,16 @@
 module joystick(
-					clk,
-					reset,
-					key,
-					led
-               );
+    clk,
+	reset,
+    key,
+	led
+);
+
 input clk,reset;
 input [7:0]key;
 output[3:0] led;
 reg[3:0] led_reg;
-reg[10:0] Count1;
-reg  Count;
 
 always@(posedge clk or negedge reset)
-    if(!reset)
-        Count1<=11'd0;
-    else
-        Count<=Count+1;
-
-always@(posedge Count or negedge reset)
     if(!reset)
         led_reg<=4'b1111;
     else if(key==8'b11111110)
@@ -36,22 +29,6 @@ always@(posedge Count or negedge reset)
         led_reg<=4'b1000;
     else if(key==8'b01111111)
         led_reg<=4'b0111;
-
-    /*
-    else if(key==16'b11111011_11111111)
-        led_reg<=4'b0100;
-    else if(key==16'b11110111_11111111)
-        led_reg<=4'b0011;
-    else if(key==16'b11101111_11111111)
-        led_reg<=4'b0010;
-    else if(key==16'b11011111_11111111)
-        led_reg<=4'b0001;
-    else if(key==16'b10111111_11111111)
-        led_reg<=4'b0000;
-    else if(key==16'b01111111_11111111)
-        led_reg<=4'b1111;
-    */
-    
     else
         led_reg<=led_reg;
 
